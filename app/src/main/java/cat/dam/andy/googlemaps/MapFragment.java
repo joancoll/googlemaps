319177cdbc1d79ef755933057bb311634fcc1cad
Joan Coll <joancoll@inspladelestany.cat>
2021-01-25 19:25:16 +0100

    Afegida actualització posició periòdica

--- app/src/main/java/cat/dam/andy/googlemaps/MapFragment.java
+++ app/src/main/java/cat/dam/andy/googlemaps/MapFragment.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +17,11 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,6 +29,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
@@ -39,6 +44,7 @@ public class MapFragment extends Fragment {
     private LocationRequest locationRequest;
     private long UPDATE_INTERVAL = 10000;  /* 10 seg */
     private long FASTEST_INTERVAL = 10000; /* 10 seg */
+    private LocationCallback locationCallback;
     private TextView tv_latitud, tv_longitud;
 
 
@@ -50,6 +56,7 @@ public class MapFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         tv_latitud =  this.getActivity().findViewById(R.id.tv_latitud);
         tv_longitud = this.getActivity().findViewById(R.id.tv_longitud);
+
 
 
         //Initialitza fragment mapa
@@ -104,6 +111,18 @@ public class MapFragment extends Fragment {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
         //Comprova permís ubicació
         if (comprovaPermisUbicacio()) {
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        // Update UI with location data
+                        // ...
+                    }
+                }
+            };
             getCurrentLocation();
         } else {
             //Si no hi ha permisos els demana
@@ -122,34 +141,62 @@ public class MapFragment extends Fragment {
             comprovaPermisUbicacio();
             return;
         }
+        //Configura l'actualització de les peticions de localització
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        //Aquest mètode estableix la velocitat en mil·lisegons en què l'aplicació prefereix rebre actualitzacions d'ubicació. Tingueu en compte que les actualitzacions d'ubicació poden ser una mica més ràpides o més lentes que aquesta velocitat per optimitzar l'ús de la bateria, o pot ser que no hi hagi actualitzacions (si el dispositiu no té connectivitat, per exemple).
+        locationRequest.setFastestInterval(5000);
+        //Aquest mètode estableix la taxa més ràpida en mil·lisegons en què la vostra aplicació pot gestionar les actualitzacions d'ubicació. A menys que la vostra aplicació es beneficiï de rebre actualitzacions més ràpidament que la taxa especificada a setInterval (), no cal que toqueu a aquest mètode.
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        /*
+        PRIORITY_BALANCED_POWER_ACCURACY - Utilitzeu aquest paràmetre per sol·licitar la precisió de la ubicació a un bloc de la ciutat, que té una precisió aproximada de 100 metres. Es considera un nivell aproximat de precisió i és probable que consumeixi menys energia. Amb aquesta configuració, és probable que els serveis d’ubicació utilitzin el WiFi i el posicionament de la torre cel·lular. Tingueu en compte, però, que l'elecció del proveïdor d'ubicació depèn de molts altres factors, com ara quines fonts estan disponibles.
+        PRIORITY_HIGH_ACCURACY - Utilitzeu aquesta configuració per sol·licitar la ubicació més precisa possible. Amb aquesta configuració, és més probable que els serveis d’ubicació utilitzin el GPS per determinar la ubicació.
+        PRIORITY_LOW_POWER - Utilitzeu aquest paràmetre per sol·licitar una precisió a nivell de ciutat, que té una precisió d'aproximadament 10 quilòmetres. Es considera un nivell aproximat de precisió i és probable que consumeixi menys energia.
+        PRIORITY_NO_POWER - Utilitzeu aquesta configuració si necessiteu un impacte insignificant en el consum d'energia, però voleu rebre actualitzacions d'ubicació quan estiguin disponibles. Amb aquesta configuració, l'aplicació no activa cap actualització d'ubicació, sinó que rep ubicacions activades per altres aplicacions.
+         */
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+
         Task<Location> taskLocation = fusedLocationProviderClient.getLastLocation();
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper());
         taskLocation.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 //quan trobi localitzacio
                 if (location != null) {
-                    //mostra posició
-                    tv_latitud.setText(String.format("%f",location.getLatitude()));
-                    tv_longitud.setText(String.format("%f",location.getLongitude()));
-                    //Sincronitza mapa
-                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            //Inicialitza Lat i Long
-                            LatLng latLng = new LatLng(location.getLatitude()
-                                    , location.getLongitude());
-                            //Crea el marcador
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Sóc aquí!");
-                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
-                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng)); //es situa a la posició
-                            googleMap.animateCamera(CameraUpdateFactory.zoomTo(MAP_ZOOM)); //ampliació extra d'aproximació
-                            googleMap.addMarker(markerOptions);
-                        }
-                    });
+                    mostrarPosicio();
                 }
 
             }
 
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //logica gestiona error
+            }
+        });
+
+    }
+    public void mostrarPosicio() {
+        //mostra posició
+        tv_latitud.setText(String.format("%f",location.getLatitude()));
+        tv_longitud.setText(String.format("%f",location.getLongitude()));
+        //Sincronitza mapa
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                //Inicialitza Lat i Long
+                LatLng latLng = new LatLng(location.getLatitude()
+                        , location.getLongitude());
+                //Crea el marcador
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Sóc aquí!");
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng)); //es situa a la posició
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(MAP_ZOOM)); //ampliació extra d'aproximació
+                googleMap.addMarker(markerOptions);
+            }
         });
     }
 
